9d1189b1829015685ab994271d2409f253fca00a
TheHiddenGamer23 <30309397+TheHiddenGamer23@users.noreply.github.com>
2017-12-04 12:52:47 -0600

    Push empty files and file structure.

---
+++ modules/utils/EventHook.py
@@ -0,0 +1,16 @@
+class _EventHook(object):
+
+    def __init__(self):
+        self.__handlers = []
+
+    def __iadd__(self, handler): # ex: onMessage += callback
+        self.__handlers.append(handler)
+        return self
+
+    def __isub__(self, handler): # ex: onMessage -= callback
+        self.__handlers.remove(handler)
+        return self
+
+    def __call__(self, *args, **keywargs): # ex: onMessage()
+        for handler in self.__handlers:
+            asyncio.get_event_loop().create_task(handler(*args, **keywargs))

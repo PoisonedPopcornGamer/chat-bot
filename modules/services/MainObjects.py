83ab93648d0841a6b960e153c32db9054f9434f5
TheHiddenGamer23 <30309397+TheHiddenGamer23@users.noreply.github.com>
2017-12-11 11:39:05 -0600

    Not Initial Commit

--- modules/services/MainObjects.py
+++ modules/services/MainObjects.py
@@ -1,12 +1,17 @@
 ##Possibly move to /modules/utils/BaseContext later.
 
-class _MainObject(object):
-    def __init__(self, attributes:dict):
+class MainObject(object):
+    def __init__(self, **attributes):
         for i in attributes:
             self.__dict__[i] = attributes[i]
 
 
 
-class Context(_MainObject):
+class Context(MainObject):
     def __getattr__(self, name): #try to get an attribute that doesn't exist, useful for being able to add anything service-specific, without throwing errors when it isn't there.
         return None
+
+
+class User(MainObject):
+    def __str__(self):
+        return str(self.id)
